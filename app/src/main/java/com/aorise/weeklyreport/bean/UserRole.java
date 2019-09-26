35dcc556ae1d5d4393c26da0d493aae7149e8e1a
jeason <975150240@qq.com>
2019-09-26 16:27:18 +0800

    1.更新字符串

--- app/src/main/java/com/aorise/weeklyreport/bean/UserRole.java
+++ app/src/main/java/com/aorise/weeklyreport/bean/UserRole.java
@@ -5,11 +5,14 @@ package com.aorise.weeklyreport.bean;
  * Date: 2019/9/25.
  */
 public class UserRole {
-    // 7 .超级管理员 //22 项目负责人 // 23 普通成员 // 25经理办 //31 销售专员
+    // 7 .超级管理员
     public static final int ROLE_SUPER_MANAGER = 7;
+    //22 项目负责人
     public static final int ROLE_PROJECT_MANAGER = 22;
+    // 23 普通成员
     public static final int ROLE_MEMBER = 23;
+    // 25经理办
     public static final int ROLE_MANAGER = 25;
-    public static final int ROLE_SALER= 31;
-
+    //31 销售专员
+    public static final int ROLE_SALER = 31;
 }
